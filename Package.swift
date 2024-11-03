fa5063c31a258d560a353d78e69cc8fb325c517e
MariusLunde <38316954+MariusLunde@users.noreply.github.com>
2024-11-03 20:28:55 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,25 @@
+// swift-tools-version:5.7
 
+import PackageDescription
+
+let package = Package(
+    name: "OSBarcodeLib",
+    platforms: [
+        .iOS(.v13)  // Specify the minimum iOS version you want to support
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/OutSystems/OSBarcodeLib-iOS.git", 
+            .upToNextMajor(from: "1.0.0") // Adjust the version according to the release you want
+        )
+    ],
+    targets: [
+        .target(
+            name: "OSBarcodeLib",
+            dependencies: [
+                .product(name: "OSBarcodeLib", package: "OSBarcodeLib-iOS")
+            ],
+            path: "Sources"  // Specify the path to your source files here
+        )
+    ]
+)
